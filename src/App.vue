90be5a00ae297528b52f551ccc2a512719e4fef6
MichellePrograms <it@petersonprograms.info>
2018-04-11 18:25:16 -0400

    added section  files

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,79 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Built-in Directives</h1>
-                <p v-text="'Some Text'"></p>
-                <p v-html="'<strong>Some strong text</strong>'"></p>
+                <h1 class="text-center">The Super Quiz</h1>
             </div>
         </div>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Custom Directives</h1>
-                <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
+                <transition name="flip" mode="out-in">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Question from './components/Question.vue';
+    import Answer from './components/Answer.vue';
+
     export default {
-        directives: {
-            'local-highlight': {
-                bind(el, binding, vnode) {
-                    var delay = 0;
-                    if (binding.modifiers['delayed']) {
-                        delay = 3000;
-                    }
-                    if (binding.modifiers['blink']) {
-                        let mainColor = binding.value.mainColor;
-                        let secondColor = binding.value.secondColor;
-                        let currentColor = mainColor;
-                        setTimeout(() => {
-                            setInterval(() => {
-                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
-                                if (binding.arg == 'background') {
-                                    el.style.backgroundColor = currentColor;
-                                } else {
-                                    el.style.color = currentColor;
-                                }
-                            }, binding.value.delay);
-                        }, delay);
-                    } else {
-                        setTimeout(() => {
-                            if (binding.arg == 'background') {
-                                el.style.backgroundColor = binding.value.mainColor;
-                            } else {
-                                el.style.color = binding.value.mainColor;
-                            }
-                        }, delay);
-                    }
-                }
+        data() {
+            return {
+                mode: 'app-question'
             }
+        },
+        methods: {
+          answered(isCorrect) {
+              if (isCorrect) {
+                  this.mode = 'app-answer';
+              } else {
+                  this.mode = 'app-question';
+                  alert('Wrong, try again!');
+              }
+          }
+        },
+        components: {
+            appQuestion: Question,
+            appAnswer: Answer
         }
     }
 </script>
 
 <style>
+    .flip-enter {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-enter-active {
+        animation: flip-in  0.5s ease-out forwards;
+    }
+
+    .flip-leave {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-leave-active {
+        animation: flip-out 0.5s ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
 </style>
