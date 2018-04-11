36f6a529954d09348e3115554c1a33ce7312ce04
MichellePrograms <it@petersonprograms.info>
2018-04-11 18:06:07 -0400

    added section 22 files

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,23 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase | to-lowercase }}</p>
-                <hr>
-                <button @click="fruits.push('Berries')">Add New Item</button>
-                <input v-model="filterText">
-                <ul>
-                    <li v-for="fruit in filteredFruits">{{ fruit }}</li>
-                </ul>
-                <hr>
-                <app-list></app-list>
-            </div>
-        </div>
-    </div>
+  <div id="app">
+    <app-header />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-    import List from './List.vue';
-    import { fruitMixin } from './fruitMixin';
-
-    export default {
-        mixins: [fruitMixin],
-        data() {
-            return {
-                text: 'Hello there!'
-            }
-        },
-        filters: {
-            toUppercase(value) {
-                return value.toUpperCase();
-            }
-        },
-        components: {
-            appList: List
-        }
+  import Header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'app-header': Header
     }
+  }
 </script>
 
 <style>
+  body, html {
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
 </style>
